c2a099a6444bd556affbd7dfd603bbeefd191620
limgeun <shing100@naver.com>
2018-12-03 23:25:36 +0900

    Feed Header 만들기

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,11 +1,29 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation";
 import FeedScreen from "../screens/FeedScreen";
 import sharedRoutes, {sharedOptions} from "./sharedRoutes";
+import { Image } from "react-native";
+import NavButton from "../components/NavButton";
 
 const HomeRoute = createStackNavigator(
     {
         Home: {
-            screen: FeedScreen
+            screen: FeedScreen,
+            navigationOptions: ({navigation}) => ({
+                headerTitle: (
+                    <Image 
+                        source={require("../assets/images/logo_black.png")} 
+                        style={{height:35}}
+                        resizeMode={"contain"}
+                    />
+                ),
+                headerLeft: (
+                    <NavButton 
+                        iconName={"ios-camera-outline"}
+                        onPress={() => navigation.navigate("TakePhoto")}
+                    />
+                )
+            })
         },
         ...sharedRoutes
     },
