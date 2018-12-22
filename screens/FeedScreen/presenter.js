157325878193e15143af3cd77ee2c02603e79866
limgeun <shing100@naver.com>
2018-12-22 17:01:23 +0900

    피드 스크린에서 피드 가져오기 기능

--- screens/FeedScreen/presenter.js
+++ screens/FeedScreen/presenter.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import PropTypes from "prop-types";
+import { View, Text, ScrollView, RefreshControl, StyleSheet } from "react-native";
 
 const FeedScreen = props => (
-    <Text onPress={() => props.navigation.navigate("Likes")}>Feed</Text>
+    <ScrollView refreshControl={
+        <RefreshControl 
+            refreshing={props.isFetching}
+            onRefresh={props.refresh}
+            tintColor={"black"}
+        />
+    }
+    contentContainerStyle={styles.containor}
+    />
 );
 
- export default FeedScreen;
+const styles = StyleSheet.create({
+    containor:{
+        flex: 1,
+        backgroundColor: "white"
+    }
+})
+
+FeedScreen.propTypes = {
+    isFetching: PropTypes.bool.isRequired
+}
+
+export default FeedScreen;
