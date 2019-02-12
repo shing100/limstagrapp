e262441ea4cbe3da40f9648b2eb62b4a68ef8d18
limgeun <shing100@naver.com>
2019-02-12 20:44:25 +0900

    SearchBar 작업 완료

--- screens/SearchScreen/index.js
+++ screens/SearchScreen/index.js
@@ -1,5 +1,24 @@
 import { connect } from "react-redux"
+import { actionCreators as photoActions } from "../../redux/modules/photos";
 import Container from "./contanier";
 
+const mapStateToProps = (state, ownProps) => {
+    const { photos: { search } } = state;
+    return {
+        search
+    };
+};
 
-export default connect()(Container);
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        getEmptyFeed: () => {
+            dispatch(photoActions.getSearch());
+        },
+        searchHashtag: hashtag => {
+            dispatch(photoActions.searchByHashtag(hashtag));
+        }
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Container);
